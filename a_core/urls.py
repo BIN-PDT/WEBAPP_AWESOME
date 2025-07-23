12df42542ec833b31aebe19b3f4406f5f746a2c1
BIN BIN <thangpd300502@gmail.com>
2025-07-23 16:28:51 +0700

    FIX UP

--- a_core/urls.py
+++ a_core/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path("category/<tag>/", home_view, name="category"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
