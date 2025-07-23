5424e9131e60e67a60e13b0f1657c964ea4f61ec
BIN BIN <thangpd300502@gmail.com>
2025-07-23 16:24:05 +0700

    COLLECT STATIC FILES

--- a_core/settings.py
+++ a_core/settings.py
@@ -66,6 +66,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -194,6 +195,8 @@ USE_TZ = True
 
 
 # STATIC FILES (CSS, JAVASCRIPT, IMAGES).
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = "static/"
 
