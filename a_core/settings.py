185baab41cb6473396b83817d1302673aea11431
BIN BIN <thangpd300502@gmail.com>
2025-07-23 19:47:38 +0700

    CONNECT MEDIA SERVER

--- a_core/settings.py
+++ a_core/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
+    "cloudinary",
     "django.contrib.sites",
     "django_cleanup.apps.CleanupConfig",
     "django_htmx",
@@ -204,6 +206,15 @@ USE_TZ = True
 
 # STATIC FILES (CSS, JAVASCRIPT, IMAGES).
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = "static/"
@@ -212,7 +223,30 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+if not DEBUG:
+    STORAGES["default"] = {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    }
+
+    CLOUDINARY_CLOUD_NAME = env("CLOUDINARY_CLOUD_NAME", None)
+    if CLOUDINARY_CLOUD_NAME is None:
+        raise ImproperlyConfigured("CLOUDINARY_CLOUD_NAME is missing!")
+
+    CLOUDINARY_API_KEY = env("CLOUDINARY_API_KEY", None)
+    if CLOUDINARY_API_KEY is None:
+        raise ImproperlyConfigured("CLOUDINARY_API_KEY is missing!")
+
+    CLOUDINARY_API_SECRET = env("CLOUDINARY_API_SECRET", None)
+    if CLOUDINARY_API_SECRET is None:
+        raise ImproperlyConfigured("CLOUDINARY_API_SECRET is missing!")
+
+    CLOUDINARY_STORAGE = {
+        "CLOUD_NAME": CLOUDINARY_CLOUD_NAME,
+        "API_KEY": CLOUDINARY_API_KEY,
+        "API_SECRET": CLOUDINARY_API_SECRET,
+    }
+else:
+    MEDIA_ROOT = BASE_DIR / "media"
 
 
 # DEFAULT PRIMARY KEY FIELD TYPE.
